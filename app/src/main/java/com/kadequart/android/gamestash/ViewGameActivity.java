2dfbe3afbf8a6ff0b5ebb6e29f4b8fc60a62fe16
Jet O'neza <jet.oneza@gmail.com>
2017-06-14 18:59:05 +0800

    Add back button to ViewGameActivity

--- app/src/main/java/com/kadequart/android/gamestash/ViewGameActivity.java
+++ app/src/main/java/com/kadequart/android/gamestash/ViewGameActivity.java
@@ -3,18 +3,32 @@ package com.kadequart.android.gamestash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import com.kadequart.android.gamestash.models.Game;
 
 public class ViewGameActivity extends AppCompatActivity {
+
+  private int gameId;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_view_game);
 
-    int id = getIntent().getIntExtra(Game.MODEL, 1);
+    gameId = getIntent().getIntExtra(Game.MODEL, 1);
 
-    Log.d("GAME ID:", id + "");
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      onBackPressed();
+
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
