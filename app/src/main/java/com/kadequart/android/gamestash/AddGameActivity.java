d9cf02ede799de7bb1cac9ee2367ce46a900a010
Jet O'neza <jet.oneza@gmail.com>
2017-06-14 16:14:29 +0800

    Add back button and change title of AddGameActivity action bar

--- app/src/main/java/com/kadequart/android/gamestash/AddGameActivity.java
+++ app/src/main/java/com/kadequart/android/gamestash/AddGameActivity.java
@@ -2,6 +2,7 @@ package com.kadequart.android.gamestash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AddGameActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class AddGameActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_add_game);
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setTitle("Add Game");
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == android.R.id.home) {
+      onBackPressed();
+
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
